e7693ddaa8ae122e8e21a60258c7680a7eef382f
Dione Rigamonti <101961735+DioneRigamonti@users.noreply.github.com>
2024-06-08 18:17:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ api = Api(app)
 
 class HelloWorld(Resource):
     def get(self):
-        return {"messege" : "Hello World"
+        return {"messege" : "Hello World"}
 
 api.add_resource(HelloWorld, '/')
 
